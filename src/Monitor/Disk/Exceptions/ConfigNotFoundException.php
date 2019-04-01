3a0d033a31f060c6c2b813dc28eaee5f9751dba0
Chou Yu Hsien <slps970093@gmail.com>
2019-04-01 22:30:07 +0800

    modify Exception

--- src/Monitor/Disk/Exceptions/ConfigNotFoundException.php
+++ src/Monitor/Disk/Exceptions/ConfigNotFoundException.php
@@ -9,7 +9,13 @@
 namespace LittleChou\SystemMonitor\Monitor\Disk\Exceptions;
 
 
+use Throwable;
+
 class ConfigNotFoundException extends \Exception
 {
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 
 }
